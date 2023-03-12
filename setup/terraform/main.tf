6e732683bddaaa8f2a5ffc5352642bd0a3b1e9d6
David Kirby <57732284+david-kirby@users.noreply.github.com>
2023-03-12 12:34:08 -0500

    fix terraform iam user policy

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -316,6 +316,11 @@ resource "aws_iam_user" "github_action_user" {
   name = "github-action-user-non-admin"
 }
 
+resource "aws_iam_user_policy" "github_action_user_permission" {
+  user   = aws_iam_user.github_action_user.name
+  policy = data.aws_iam_policy_document.github_policy.json
+}
+
 data "aws_iam_policy_document" "github_policy" {
   statement {
     effect    = "Allow"
