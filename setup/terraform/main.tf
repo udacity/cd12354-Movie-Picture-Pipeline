1f2764a66e652eeb575c61c2118a7574ec53b978
linkinf88k <linkinf88k@gmail.com>
2024-02-16 15:45:33 +0700

    update

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -316,15 +316,20 @@ resource "aws_iam_user" "github_action_user" {
   name = "github-action-user"
 }
 
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
-
 data "aws_iam_policy_document" "github_policy" {
   statement {
     effect    = "Allow"
     actions   = ["ecr:*", "eks:*", "ec2:*"]
     resources = ["*"]
   }
+}
+
+resource "aws_iam_policy" "github_policy" {
+  name   = "github-policy"
+  policy = data.aws_iam_policy_document.github_policy.json
+}
+
+resource "aws_iam_user_policy_attachment" "github_policy" {
+  user       = aws_iam_user.github_action_user.name
+  policy_arn = aws_iam_policy.github_policy.arn
 }
