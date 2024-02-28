6cc9fc54b44c263b1c6ecf8e2c12fdfec5c2a589
Joseph Isibor <joseph.isibor1@natwest.com>
2024-02-28 22:52:23 +0000

    revert last commit

--- setup/init.sh
+++ setup/init.sh
@@ -3,24 +3,15 @@ set -e -o pipefail
 
 echo "Fetching IAM github-action-user ARN"
 userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
-echo $userarn
 
-# Define the version of aws-iam-authenticator
-IAM_AUTHENTICATOR_VERSION="0.6.3"
-IAM_AUTHENTICATOR_URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${IAM_AUTHENTICATOR_VERSION}/aws-iam-authenticator_${IAM_AUTHENTICATOR_VERSION}_linux_amd64"
-
-# Download and install aws-iam-authenticator
-echo "Downloading aws-iam-authenticator..."
-curl -L "$IAM_AUTHENTICATOR_URL" -o aws-iam-authenticator
+# Download tool for manipulating aws-auth
+echo "Downloading tool..."
+curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
-# Update permissions
-echo "Updating permissions..."
-# ./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
-kubectl config set-credentials github-action-role --exec-command=aws-iam-authenticator --exec-api-version=client.authentication.k8s.io/v1alpha1 --exec-arg="token" --exec-arg="-i" --exec-arg="cluster"
+echo "Updating permissions"
+./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
 
-# Clean up
-echo "Cleaning up..."
+echo "Cleaning up"
 rm aws-iam-authenticator
-
 echo "Done!"
