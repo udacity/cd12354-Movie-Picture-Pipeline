cc2427b43ca76e51af2d5e6d84d71b12ff8cfe13
Sean Healey <sean@digitalworkshop.net>
2024-03-25 00:01:42 +0000

    make init.sh osx-friendly

--- setup/init.sh
+++ setup/init.sh
@@ -2,11 +2,11 @@
 set -e -o pipefail
 
 echo "Fetching IAM github-action-user ARN"
-userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
+userarn=$(aws iam get-user --user-name github-action-user | grep "\"Arn\"" | sed 's/.*"\(arn:aws:iam.*\)",/\1/')
 
 # Download tool for manipulating aws-auth
 echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
+curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.14/aws-iam-authenticator_0.6.14_darwin_amd64 -o aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
 echo "Updating permissions"
