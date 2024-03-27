f88fc8697b5147ef3782e661f1d4eec479df2e4f
wmorak@gmail.com <housenos@outlook.com>
2024-03-27 01:47:02 +0000

    feature/frontend update

--- starter/backend/movies/resources.py
+++ starter/backend/movies/resources.py
@@ -14,8 +14,8 @@ class Movies(MethodView):
     def get(self, movie_id):
         if movie_id is None:
             # Return a list of all movies
-            return jsonify({"movies": [dict({"title": movie["title"]}, **{"id": i})
-                for i, movie in movies.items()]})
+            return jsonify({"movies": [dict({"title": movie["title"]}, 
+            **{"id": i})for i, movie in movies.items()]})
         else:
             # Return the details of a specific movie
             return jsonify({"movie": movies[str(movie_id)]})
