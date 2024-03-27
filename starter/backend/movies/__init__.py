85f9b0fadd85b7c9442f26f6431865ad39319b46
wmorak@gmail.com <housenos@outlook.com>
2024-03-27 02:06:28 +0000

    feature/frontend -update

--- starter/backend/movies/__init__.py
+++ starter/backend/movies/__init__.py
@@ -6,7 +6,8 @@ movies = Movies.as_view("movies")
 
 movies_api.add_url_rule("/movies", strict_slashes=False,
                             defaults={"movie_id": None},
-                            view_func=movies, methods=["GET"])
+                        view_func=movies, methods=["GET"])
+                        
 movies_api.add_url_rule("/movies", view_func=movies, methods=["POST"])
 movies_api.add_url_rule("/movies/<int:movie_id>", 
     view_func=movies, methods=["GET", "PUT", "DELETE"])
